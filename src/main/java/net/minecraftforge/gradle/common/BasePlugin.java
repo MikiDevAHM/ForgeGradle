91862066505e57d7266f5d7b04beff3c8e2d08d9
MikiDevAHM <miki@mittelsdorf.at>
2026-05-14 10:30:16 +0200

    fix: mvn url

--- src/main/java/net/minecraftforge/gradle/common/BasePlugin.java
+++ src/main/java/net/minecraftforge/gradle/common/BasePlugin.java
@@ -228,7 +228,7 @@ public abstract class BasePlugin<K extends BaseExtension> implements Plugin<Proj
         project.getRepositories().maven(mavenArtifactRepository -> {
             try {
                 mavenArtifactRepository.getMetadataSources().artifact(); // mappings don't have pom
-                mavenArtifactRepository.setUrl(new URL("https://maven.neoforged.net/releases"));
+                mavenArtifactRepository.setUrl(new URL("https://maven.neoforged.net/"));
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
